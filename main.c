c5a706077dc776e6f1c3dddbf5c3eae1b49d9196
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-11-11 12:30:20 +0600

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int i, l;
+    char s[1000];
+
+    while(gets(s)) {
+        l = strlen(s);
+        for(i=0; i<l; i++) {
+            printf("%c",s[i]-7);
+        }
+        printf("\n");
+    }
+    return 0;
+}
